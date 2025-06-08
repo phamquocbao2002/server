b4731098972856d26222e59ed98c3f8320a01577
Bao Pham <bao.pham@globalcare.vn>
2025-06-08 18:30:54 +0700

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Cấu hình nơi lưu ảnh
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "objectAvt/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  }
+});
+
+const upload = multer({ storage });
+
+app.use(express.static(".")); // Cho phép truy cập admin.html, object.html
+
+// API upload
+app.post("/upload", upload.single("avt"), (req, res) => {
+  const filePath = "/objectAvt/" + req.file.filename;
+  res.json({ success: true, filePath });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
