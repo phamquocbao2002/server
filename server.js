0249e53ee7874eca412951cb87e1685949131ba9
Bao Pham <bao.pham@globalcare.vn>
2025-06-08 18:58:54 +0700

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Cấu hình nơi lưu ảnh
 const storage = multer.diskStorage({
@@ -27,5 +27,5 @@ app.post("/upload", upload.single("avt"), (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
